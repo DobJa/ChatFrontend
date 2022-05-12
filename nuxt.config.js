6d1d168f83644114d6252ae55c6a4b582200f7fa
DobJa <jandobrosolski@gmail.com>
2022-05-12 23:54:29 +0200

    cors workaround?

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,6 +41,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
+    proxyHeaders: "header",
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
