b55ac35611a2af5168d0053b104b7f89a8536d0d
DobJa <jandobrosolski@gmail.com>
2022-05-12 22:32:47 +0200

    rebuild chatpage, added image input

--- components/ChatPage.vue
+++ components/ChatPage.vue
@@ -1,13 +1,31 @@
 <template>
 <div>
              <div>
-        <b-form-input id="message"
+        <!-- <b-form-input id="message"
                       type="text"
                       v-model="message"
                       required
                       @keyup.native.enter="send"
                       placeholder="Type your message">
+        </b-form-input> -->
+
+<b-form @submit="onSubmit">
+  <b-input-group prepend="Chat input my dood" class="mb-2">
+        <b-form-input id="message"
+                      type="text"
+                      v-model="message"
+                      required
+                      placeholder="Type your message">
         </b-form-input>
+        <b-form-file
+            v-model="file1"
+            :state="Boolean(file1)"
+            placeholder="Choose a file or drop it here..."
+            drop-placeholder="Drop file here..."
+        ></b-form-file>
+              <b-button type="submit" variant="primary">Submit</b-button>
+  </b-input-group>
+  </b-form>
 
         <div class="messages-container" v-html="chat">
         </div>
