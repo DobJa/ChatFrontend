22e1b01307fd11bcff4d7a5d4afe6227437fd504
Dawid Osuchowski <dwdosuchowski@gmail.com>
2022-05-25 01:29:03 +0200

    fix API baseUrls

--- components/WebSocket.js
+++ components/WebSocket.js
@@ -3,7 +3,7 @@ import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr'
 export default {
     createHub() {
         return new HubConnectionBuilder()
-            .withUrl("http://localhost:8080/chat")
+            .withUrl("http://relay.test/chat")
             .configureLogging(LogLevel.Information)
             .build();
     }
