74db8fed7268ac1205a4d81737abbb8b8648f5da
DobJa <jandobrosolski@gmail.com>
2022-05-25 23:40:18 +0200

    changed the way messages are stored and displayed, added object support

---
+++ components/Chat.vue
@@ -0,0 +1,52 @@
+<template>
+ <div class="chat-wrapper">
+
+    <div class="chat__form">
+     <ChatForm />
+   </div>
+
+   <div class="chat" ref="chat">
+     <Message
+       v-for="(misag) in messages.slice().reverse()"
+       :key="misag.MessId"
+       :User="misag.User"
+       :text="misag.text"
+       :timestamp="misag.timestamp"
+     />
+   </div>
+
+ </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+import Message from "@/components/Message";
+import ChatForm from "@/components/ChatForm";
+
+export default {
+ components: {
+   Message,
+   ChatForm
+ },
+ head() {
+   return {
+     title: `Chat Room`
+   };
+ },
+  methods: {
+   ...mapMutations(["newMessage"])
+ },
+ computed: {
+   ...mapState(["user", "messages"])
+ },
+ watch: {
+   messages() {
+     setTimeout(() => {
+       if (this.$refs.chat) {
+         this.$refs.chat.scrollTop = this.$refs.chat.scrollHeight;
+       }
+     }, 0);
+   }
+ }
+};
+</script>
