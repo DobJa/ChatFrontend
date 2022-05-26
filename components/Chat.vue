39d72e019a3c09298e01fe0268666eb7edcb95a3
DobJa <jandobrosolski@gmail.com>
2022-05-26 21:51:52 +0200

    squash! fixy of user display>

--- components/Chat.vue
+++ components/Chat.vue
@@ -8,8 +8,9 @@
    <div class="chat" ref="chat">
      <Message
        v-for="(misag) in messages.slice().reverse()"
-       :key="misag.MessId"
-       :User="misag.User"
+
+       :key="misag.messid"
+       :User="misag.user"
        :text="misag.text"
        :timestamp="misag.timestamp"
      />
